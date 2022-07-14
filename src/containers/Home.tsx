52bb3dd3fa022befae106801a654386bfeedeae3
Sebastian Sierra <SebastianSierra005@gmail.com>
2022-07-13 23:05:49 -0500

    [add] firebase hosting

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 import Products from "../components/Products"
-import initialState from "../initialState"
 
 const Home:React.FC = () => {
   return (
-    <Products products={initialState.products} />
+    <Products />
   )
 }
 
